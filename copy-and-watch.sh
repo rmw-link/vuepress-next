5c7209e2c4b5369c96eb5f9400bd29e280e574bd
gcxfd <i@rmw.link>
2021-11-28 04:44:19 +0800

    🔶

--- copy-and-watch.sh
+++ copy-and-watch.sh
@@ -4,4 +4,4 @@ DIR=$(cd "$(dirname "$0")"; pwd)
 set -ex
 cd $DIR
 
-npx copy-and-watch packages/@vuepress/theme-default ../blog-vuepress2/node_modules/@vuepress/theme-default --watch
+npx concurrently --kill-others "npx copy-and-watch packages/@vuepress/theme-default ../blog-vuepress2/node_modules/@vuepress/theme-default --watch" "yarn dev"
