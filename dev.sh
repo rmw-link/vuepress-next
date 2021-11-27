81790fd903932cc43f914c06cd4565521f49672d
gcxfd <i@rmw.link>
2021-11-28 05:36:51 +0800

    🔶

---
+++ dev.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+set -ex
+cd $DIR
+
+npx concurrently --kill-others "npx copy-and-watch packages/@vuepress/theme-default ../blog-vuepress2/node_modules/@vuepress/ --watch" "yarn dev"
--- packages/@vuepress/theme-default/src/client/file/font/css/readme.md
+++
@@ -1,5 +0,0 @@
-中文字体 noto sans sc sliced
-英文字体 lato
-用 ./download.xsh 下载
-
-
